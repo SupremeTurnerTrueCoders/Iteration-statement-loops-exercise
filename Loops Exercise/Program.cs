7fec634042e933ce34b743ef713d4547a5b6c2d8
SupremeTurnerTrueCoders <email@example.com>
2024-04-17 17:40:21 -0400

    Completed Iteration and loops exercise

---
+++ Loops Exercise/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata;
+
+Console.WriteLine("Hello, World!");
+
+
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"{i}");
+    
+}
+
+for (int i = 0;i < 10; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; ; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i  = 0; ; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; ; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; ; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; ; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; ; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; ; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; ; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; ; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; ; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; ; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; ; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; ; i++)
+{
+    Console.WriteLine($"{i}");
+}
